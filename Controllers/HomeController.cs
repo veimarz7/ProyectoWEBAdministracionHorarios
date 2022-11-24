6403d3b7b12beb4838f5987a4e9194b8686aeecd
t800 genesis <t-800genesis@hotmail.com>
2022-11-24 10:42:39 -0400

    agregando html

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace ProyectoWEBAdministracionHorarios.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         public IActionResult RegistroEmpleado()
         {
             return View();
